37fe9204c50170373a120ce124e803082c45ce2a
Syed Haider Ali <154678645+Syedhaideali@users.noreply.github.com>
2024-03-05 03:53:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var math =  +prompt('your maths number')   
+var urdu = +prompt('your urdu number')
+console.log (typeof math)
+console.log(math + urdu)
+var percentage =( math + urdu) /300 * 100
+console.log( 'your percentage is ' + percentage + '%')
+   document.write( " <h1>assignment<h1>"  )
